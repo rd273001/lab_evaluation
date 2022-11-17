a52842a4a6118fefdec995c741d29a6507475672
Ravi Dubey <84777009+rd273001@users.noreply.github.com>
2022-11-17 13:58:59 +0530

    ReUpdate Hello1.java

--- Hello1.java
+++ Hello1.java
@@ -1,5 +1,5 @@
 public class Hello1 {
    public static void main(String[] Args) {
-       System.out.println("Hello1 modified in local to see the change reflected in jenkins build #2./nHello1 modified and pushed to remote to get build automatically on jenkins as change made in file and pushed to remote as scheduled poll SCM.");
+       System.out.println("Hello1 modified in remote to see the change reflected in jenkins build #3.\nHello1 modified and pulled to local to get build automatically on jenkins as change made in file and pulled to local as scheduled poll SCM.");
    }
 }
