37c15743b47403a7198753cf033d5eabf30039bd
rd273001 <rd273001@gmail.com>
2022-11-17 13:47:47 +0530

    Hello1.java modified

--- Hello1.java
+++ Hello1.java
@@ -1,5 +1,5 @@
 public class Hello1 {
    public static void main(String[] Args) {
-       System.out.println("Hello1 modified from remote to see the change reflected in local.");
+       System.out.println("Hello1 modified in local to see the change reflected in jenkins build #2./nHello1 modified and pushed to remote to get build automatically on jenkins as change made in file and pushed to remote as scheduled poll SCM.");
    }
 }
